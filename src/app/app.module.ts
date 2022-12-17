f1566234772c4dae9509a1b17df62a510e62b875
Renato Martins <renatomartinsti@gmail.com>
2022-12-17 14:25:41 -0300

    first page home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ExtraOptions, RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './features/home/home.component';
+import { HomeModule } from './features/home/home.module';
+
+const routes: Routes = [{ path: '', component: HomeComponent }];
+
+const extraOptions: ExtraOptions = { useHash: true }; //{ enableTracing: true, useHash: true };
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(routes, extraOptions),
+    BrowserModule,
+    HomeModule,
   ],
+  exports: [RouterModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
