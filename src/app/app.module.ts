071c02f05dfbe63c0ad485222465cb22b1953c84
Renato Martins <renatomartinsti@gmail.com>
2022-12-17 23:04:46 -0300

    adjust app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
-import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ExtraOptions, RouterModule, Routes } from '@angular/router';
 
+import { MatCardModule } from '@angular/material/card';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 //import { RouteEventsService } from './core/services/route-events.service';
-import { HomeComponent } from './features/home/home.component';
 import { HomeModule } from './features/home/home.module';
+import { ValueTransferRoutingModule } from './features/value-transfer/value-transfer-routing.module';
+import { ValueTransferModule } from './features/value-transfer/value-transfer.module';
 
 const routes: Routes = [{ path: '',  loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) }];
 
@@ -19,8 +21,10 @@ const extraOptions: ExtraOptions = { useHash: true }; //{ enableTracing: true, u
     RouterModule.forRoot(routes, extraOptions),
     BrowserModule,
     BrowserAnimationsModule,
+    MatCardModule,
     HomeModule,
     CoreModule,
+    ValueTransferModule,
   ],
   exports: [RouterModule],
   // providers: [
